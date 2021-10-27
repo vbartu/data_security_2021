b45918515e0eb8d8972c73ecdc2295cd35dc1d31
Vicente Bartual <vicentebartualf@gmail.com>
2021-10-27 14:06:53 +0200

    Fix token type

--- src/TokenChecker.java
+++ src/TokenChecker.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 
 class TokenInfo {
+	static final int VALIDITY_MS = 5*60*1000;
+
 	public String user;
 	public Date validity;
 
 	public TokenInfo(String user) {
 		this.user = user;
-		this.validity = new Date(System.currentTimeMillis());
+		this.validity = new Date(System.currentTimeMillis() + VALIDITY_MS);
 	}
 }
 
@@ -17,11 +19,18 @@ public class TokenChecker {
 
 	static final int TOKEN_LENGTH = 16; // 128 bits
 
-	private HashMap<byte[], TokenInfo> sessions;
+	private HashMap<String, TokenInfo> sessions;
 	private SecureRandom rd;
 
+	private String bytesToHex(byte[] a) {
+		StringBuilder sb = new StringBuilder(a.length * 2);
+		for(byte b: a)
+			sb.append(String.format("%02x", b));
+		return sb.toString();
+	}
+
 	public TokenChecker() {
-		this.sessions = new HashMap<byte[], TokenInfo>();
+		this.sessions = new HashMap<>();
 		this.rd = new SecureRandom();
 	}
 
@@ -29,23 +38,21 @@ public class TokenChecker {
 	public String newSession(String user) {
 		byte[] token = new byte[TOKEN_LENGTH];
 		this.rd.nextBytes(token);
-		this.sessions.put(token, new TokenInfo(user));
+		String stringToken = this.bytesToHex(token);
+		this.sessions.put(stringToken, new TokenInfo(user));
 
-		return new String(token);
+		return stringToken;
 	}
 
 	public boolean checkToken(String token) {
-		byte[] tokenByte = token.getBytes();
 		TokenInfo tokenInfo = this.sessions.get(token);
 		if (tokenInfo == null) {
 			return false;
 		}
-
 		Date now = new Date(System.currentTimeMillis());
 		if (now.compareTo(tokenInfo.validity) > 0) {
 			return false;
 		}
-
 		return true;
 	}
 }
